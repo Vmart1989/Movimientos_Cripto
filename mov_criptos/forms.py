87544752047fc88cbf6a20c368bb5c9915e142f0
Victor Martinez <victor.martinezsegura@gmail.com>
2023-01-23 23:59:27 +0100

    iniciada la estructura de form

--- mov_criptos/forms.py
+++ mov_criptos/forms.py
@@ -1,18 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, FloatField, SubmitField, TimeField
+from wtforms import StringField, DateField, FloatField, SubmitField, TimeField, SelectMultipleField
 from wtforms.validators import DataRequired, Length, ValidationError
 
 class RegistrosForm(FlaskForm):
    date = DateField('Fecha')
    time = TimeField('Fecha')
-   moneda_from = StringField('From', validators=[DataRequired(message="Elige moneda")])
-   moneda_to = StringField('To', validators=[DataRequired(message="Elige moneda")])
-   cantidad_from = FloatField('Cantidad From', validators=[DataRequired(message="Ingresa cantidad")])
-   
+   moneda_from = SelectMultipleField('Moneda a cambiar', choices=[('EUR'), ('BTC'), ('ETH'),('USDT'), ('BNB'), ('XRP'),('ADA'), ('SOL'), ('DOT'),('MATIC')])
+   moneda_to = SelectMultipleField('Moneda deseada', choices=[('EUR'), ('BTC'), ('ETH'),('USDT'), ('BNB'), ('XRP'),('ADA'), ('SOL'), ('DOT'),('MATIC')])
+   cantidad_from = FloatField('Cantidad', validators=[DataRequired(message="Ingresa cantidad")])
    calcular = SubmitField('Calcular')
    
-   cantidad_to = FloatField('Cantidad To')
+   cantidad_to = FloatField('Valor de cambio actual')
 
    p_u = StringField('Precio Unitario')
    
-   submit = SubmitField('Aceptar')
+   submit = SubmitField('Realizar transacción')
